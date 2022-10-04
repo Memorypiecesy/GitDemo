a5fd052b29484644229e056dfcbb40fe7a0230fb
Memorypieces <yjiyisuipian@outlook.com>
2022-10-04 16:22:22 +0800

    implement: buttonAdd onClick listener
    实现了"buttonAdd"按钮的监听，点击后文本显示的数字会++

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.gitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = "0"
+        }
+        findViewById<Button>(R.id.buttonAdd).apply {
+            setOnClickListener {
+                textView.text = "${++number}"
+            }
+        }
     }
 }
