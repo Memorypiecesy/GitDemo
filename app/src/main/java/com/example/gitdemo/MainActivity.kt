492de5898a3c37c428ee1721a1c1011bba7c587b
Memorypieces <yjiyisuipian@outlook.com>
2022-10-05 12:02:44 +0800

    implement: options menu reset item selected

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -3,18 +3,20 @@ package com.example.gitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private var number = 0
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         number = savedInstanceState?.getInt("NUMBER")?:0
-        val textView = findViewById<TextView>(R.id.textView).apply {
+        textView = findViewById<TextView>(R.id.textView).apply {
             text = "$number"
         }
         findViewById<Button>(R.id.buttonAdd).apply {
@@ -27,6 +29,14 @@ class MainActivity : AppCompatActivity() {
                 textView.text = "${--number}"
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId==R.id.menu_reset){
+            number=0
+            textView.text = "$number"
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
