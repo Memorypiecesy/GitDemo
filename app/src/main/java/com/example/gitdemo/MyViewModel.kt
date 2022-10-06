1ca0b78a4db58ff190aae1540267d8a0be552618
Memorypieces <yjiyisuipian@outlook.com>
2022-10-06 09:06:42 +0800

    refactor: use LiveData in ViewModel

--- app/src/main/java/com/example/gitdemo/MyViewModel.kt
+++ app/src/main/java/com/example/gitdemo/MyViewModel.kt
@@ -1,7 +1,18 @@
 package com.example.gitdemo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel:ViewModel() {
-    var number = 0
+    private val _number = MutableLiveData(0)
+    val numberLiveData:LiveData<Int> = _number
+
+    fun add(number:Int){
+        _number.value = _number.value?.plus(number)
+    }
+
+    fun reset(){
+        _number.value = 0
+    }
 }
